41341aa0bd6bb7be38efa727abce26a405046a4e
Humberto Dias <hldtux@gmail.com>
2019-05-05 12:55:25 +0200

    Glossary fixed

--- src/views/Glossary.vue
+++ src/views/Glossary.vue
@@ -3,8 +3,24 @@
   <IonVuePage title="Glosario">
 
   <ion-toolbar>
+
+    <ion-item lines="none">
+        <ion-grid>
+          <ion-row>
+            <ion-col><ion-img src="flags/es.png"></ion-img></ion-col>
+            <ion-col><ion-img src="flags/en.png"></ion-img></ion-col>
+            <ion-col><ion-img src="flags/fr.png"></ion-img></ion-col>
+            <ion-col><ion-img src="flags/pt.png"></ion-img></ion-col>
+            <ion-col><ion-img src="flags/he.png"></ion-img></ion-col>
+            <ion-col><ion-img src="flags/tk.png"></ion-img></ion-col>
+            <ion-col><ion-img src="flags/ar.png"></ion-img></ion-col>
+          </ion-row>
+        </ion-grid>
+    </ion-item>
+
     <ion-searchbar placeholder="Filtro" @input="handleInput">
     </ion-searchbar>
+
   </ion-toolbar>
 
     <ion-content>
@@ -16,12 +32,13 @@
             </ion-card-title>
           </ion-card-header>
           <ion-card-content>
-            <h3>{{item[1]}}</h3>
+            <!--<h3>{{item[1]}}</h3>-->
             <h3>{{item[2]}}</h3>
             <h3>{{item[3]}}</h3>
             <h3>{{item[4]}}</h3>
             <h3>{{item[5]}}</h3>
             <h3>{{item[6]}}</h3>
+            <h3>{{item[7]}}</h3>
           </ion-card-content>
         </ion-card>
 
