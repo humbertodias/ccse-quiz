b6a210d4f12fafe994292e6785fe5eccd92beb54
Humberto Dias <hldtux@gmail.com>
2019-05-05 16:56:54 +0200

    Centralizing url's

--- src/views/Info.vue
+++ src/views/Info.vue
@@ -1,59 +1,56 @@
 <template>
-
   <IonVuePage title="Información">
     <ion-content>
-      
       <ion-card>
-        <ion-item href="pdf/especificaciones_prueba.pdf">
+        <ion-item :href="Config.PDF_ESPEC">
           <ion-label>Especificaciones de la prueba</ion-label>
           <ion-icon name="document" slot="end"></ion-icon>
         </ion-item>
       </ion-card>
 
       <router-link to="glossary">
-      <ion-card>
-        <ion-item>
-          <ion-label>Glosario Multilingue</ion-label>
-          <ion-icon name="arrow-dropright-circle" slot="end"></ion-icon>
-        </ion-item>
-      </ion-card>
+        <ion-card>
+          <ion-item>
+            <ion-label>Glosario Multilingue</ion-label>
+            <ion-icon name="arrow-dropright-circle" slot="end"></ion-icon>
+          </ion-item>
+        </ion-card>
       </router-link>
 
       <router-link to="links">
-      <ion-card>
-        <ion-item>
-          <ion-label>Enlaces</ion-label>
-          <ion-icon name="arrow-dropright-circle" slot="end"></ion-icon>
-        </ion-item>
-      </ion-card>
+        <ion-card>
+          <ion-item>
+            <ion-label>Enlaces</ion-label>
+            <ion-icon name="arrow-dropright-circle" slot="end"></ion-icon>
+          </ion-item>
+        </ion-card>
       </router-link>
 
       <router-link to="acronyms">
-      <ion-card>
-        <ion-item>
-          <ion-label>Acrónimos</ion-label>
-          <ion-icon name="arrow-dropright-circle" slot="end"></ion-icon>
-        </ion-item>
-      </ion-card>
+        <ion-card>
+          <ion-item>
+            <ion-label>Acrónimos</ion-label>
+            <ion-icon name="arrow-dropright-circle" slot="end"></ion-icon>
+          </ion-item>
+        </ion-card>
       </router-link>
 
-
       <ion-card>
-        <ion-item href="pdf/inventario.pdf">
+        <ion-item :href="Config.PDF_INVENTORY">
           <ion-label>Inventario general de contenidos</ion-label>
           <ion-icon name="document" slot="end"></ion-icon>
         </ion-item>
       </ion-card>
 
       <ion-card>
-        <ion-item href="pdf/modelo_examen.pdf">
+        <ion-item :href="Config.PDF_MODEL">
           <ion-label>Modelo de prueba CCSE</ion-label>
           <ion-icon name="document" slot="end"></ion-icon>
         </ion-item>
       </ion-card>
 
       <ion-card>
-        <ion-item href="pdf/manual.pdf">
+        <ion-item :href="Config.PDF_MANUAL">
           <ion-label>
             Manual CCSE
             <p>preparación para los exámenes</p>
@@ -61,12 +58,10 @@
 
           <ion-icon name="document" slot="end"></ion-icon>
         </ion-item>
-
-
       </ion-card>
 
       <ion-card>
-        <ion-item href="https://examenes.cervantes.es/es/ccse">
+        <ion-item :href="Config.CCSE_URL">
           <ion-label>
             Sitio web oficial
             <p>examenes.cervantes.es/es/ccse</p>
@@ -74,11 +69,26 @@
           <ion-icon name="globe" slot="end"></ion-icon>
         </ion-item>
       </ion-card>
-
     </ion-content>
-
   </IonVuePage>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      Config: {
+        PDF_ESPEC: "https://examenes.cervantes.es/sites/default/files/especificaciones_prueba_ccse.pdf",
+        PDF_INVENTORY: "https://examenes.cervantes.es/sites/default/files/inventario_general_contenidos_prueba_CCSE.pdf",
+        PDF_MANUAL: "https://examenes.cervantes.es/sites/default/files/ccse_manual_2019.pdf",
+        PDF_MODEL: "https://examenes.cervantes.es/sites/default/files/Modelo-CCSE-2018.pdf",
+        CCSE_URL: "https://examenes.cervantes.es/es/ccse"
+      }
+    };
+  }
+};
+</script>
+
 
 <style>
 a {
