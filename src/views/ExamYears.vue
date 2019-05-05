370964f0e65bc2db1fa338be91e1c98f0539a3bd
Humberto Dias <hldtux@gmail.com>
2019-05-05 17:37:54 +0200

    Simplifying Exam by years

--- src/views/ExamYears.vue
+++ src/views/ExamYears.vue
@@ -23,12 +23,10 @@ export default {
     return {
       itemsOriginal: [],
       items: [],
-      questions: []
     };
   },
   mounted: function() {
     this.loadExams();
-    this.loadQuestions();
   },
   methods: {
     loadExams(){
@@ -43,35 +41,16 @@ export default {
         this.items = this.itemsOriginal;
       });
     },
-    loadQuestions(){
-      fetch("preguntas.json", {
-        method: "get"
-      })
-      .then(response => {
-        return response.json();
-      })
-      .then(jsonData => {
-        this.questions = jsonData.results;
-      });
-    },
     handleInput(evt) {
       let searchTerm = evt.srcElement.value;
       this.items = this.itemsOriginal.filter(item => {
         return item[0].toLowerCase().indexOf(searchTerm.toLowerCase()) > -1;
       });
     },
-    questionIds(qIndexes) {
-      let ids = [];
-      for (let qIdx of qIndexes) {
-        ids.push(this.questions[qIdx].id);
-      }
-      return ids;
-    },
-    goToQuestionary(qIndexes) {
-      let ids = this.questionIds(qIndexes);
+    goToQuestionary(qIds) {
       this.$router.push({
         name: "questions",
-        params: { ids: ids }
+        params: { ids: qIds }
       });
     }
   }
