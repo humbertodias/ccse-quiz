c0990f55932d6dd58a1ba3c48e16a83347760fa2
Humberto Dias <hldtux@gmail.com>
2019-05-09 22:16:42 +0200

    Speech rate 1.0 as talking normal

--- src/speech.js
+++ src/speech.js
@@ -1,6 +1,7 @@
+// Mobile
 import { TextToSpeech } from '@ionic-native/text-to-speech';
 
-// Using the /build/artyom.js file
+// Browser
 import Artyom from 'artyom.js';
 const artyom = new Artyom();
 
@@ -8,46 +9,34 @@ export default {
   components: {
     TextToSpeech
   },
+  LANG: 'es-ES',
+  RATE: 1.0,
   say(text) {
-    if (this.isMobile()) {
-      let options = {
-        text: text,
-        locale: 'es-ES',
-        rate: 0.75
-      };
-      TextToSpeech.speak(options)
-      .then(() => console.log('Success'))
-      .catch((reason) => console.log(reason));
-    }
-    else{
-      artyom.say(text);
-    }
+    this.isMobile() ?
+      this.sayMobile(text) :
+      this.sayBrowser(text);
   },
   canSay() {
     return true;
-  }, isMobile() {
+  },
+  isMobile() {
     return !artyom.speechSupported();
-  }
+  },
+  sayMobile(text) {
+    let options = {
+      text: text,
+      locale: this.LANG,
+      rate: this.RATE
+    };
+    TextToSpeech
+      .speak(options)
+      .catch((reason) => console.log(reason));
+  },
+  sayBrowser(text) {
+    let options = {
+      lang: this.LANG,
+      speed: this.RATE
+    };
+    artyom.say(text, options);
+  },
 };
-
-function initSpeech() {
-  if (!artyom.speechSupported()) {
-    console.log("speech is not supported!");
-    return;
-  }
-  console.log('initSpeech')
-  artyom.fatality();// use this to stop any of
-  setTimeout(function () {// if you use artyom.fatality , wait 250 ms to initialize again.
-    artyom.initialize({
-      lang: 'es-ES',// A lot of languages are supported. Read the docs !
-      continuous: true,// Artyom will listen forever
-      listen: false, // Start recognizing
-      debug: true, // Show everything in the console
-      speed: 1 // talk normally
-    }).then(function () {
-      console.log("Ready to work !");
-    });
-  }, 250);
-}
-
-initSpeech();
