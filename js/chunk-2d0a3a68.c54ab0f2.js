23be6d3bd2b9bfb853e174a2d54c0ac87aeeb315
Humberto <hldtux@gmail.com>
2022-10-09 12:49:38 +0200

    deploy

--- js/chunk-2d0a3a68.c54ab0f2.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0a3a68"],{"02a1":function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return i}));var o=e("69ed"),i=(e("b6d2"),function(){var n=window;n.addEventListener("statusTap",(function(){Object(o["g"])((function(){var t=n.innerWidth,e=n.innerHeight,i=document.elementFromPoint(t/2,e/2);if(i){var c=i.closest("ion-content");c&&c.componentOnReady().then((function(){Object(o["m"])((function(){return c.scrollToTop(300)}))}))}}))}))})}}]);
-//# sourceMappingURL=chunk-2d0a3a68.c54ab0f2.js.map
