7a8dfb160ea09afcbf45cde7eb799c594be05f1a
Humberto <hldtux@gmail.com>
2022-10-09 12:36:23 +0200

    gh-pages

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ cd dist
 git init
 git add -A
 git commit -m 'New Deployment'
-git push -f git@github.com:humbertodias/ccse-quiz.git master:gh-pages
+git push -f git@github.com:humbertodias/ccse-quiz.git main:gh-pages
 
 cd -
