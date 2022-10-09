7c631eb3cb521f0be5037dc2704d385be0da6619
Humberto Dias <hldtux@gmail.com>
2022-10-10 00:03:54 +0200

    icons

---
+++ src/icons.js
@@ -0,0 +1,20 @@
+// icons
+import { add, volumeHigh, clipboard, helpCircleOutline, help, timer, stats, informationCircleOutline, thumbsUp, document, arrowDroprightCircle, globe, closeCircle, home } from "ionicons/icons";
+import { addIcons } from "ionicons";
+addIcons({
+  "ios-add": add.ios,
+  "md-add": add.md,
+  "md-clipboard": clipboard.md,
+  "md-volume-high": volumeHigh.md,
+  "md-help-circle-outline": helpCircleOutline.md,
+  "md-help": help.md,
+  "md-timer": timer.md,
+  "md-stats": stats.md,
+  "md-information-circle-outline": informationCircleOutline.md,
+  "md-thumbs-up": thumbsUp.md,
+  "md-document": document.md,
+  "md-arrow-dropright-circle": arrowDroprightCircle.md,
+  "md-globe": globe.md,
+  "md-close-circle": closeCircle.md,
+  "md-home": home.md,
+});
--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
-
-Vue.config.productionTip = false;
+import './icons';
 
 // Import IonVuePage component
 import IonVuePage from '@/components/IonVuePage.vue';
