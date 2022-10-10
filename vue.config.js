737e78304ff9a33a29b8712067b9aa4de85d4063
Humberto Dias <hldtux@gmail.com>
2022-10-10 18:56:35 +0200

    maximumFileSizeToCacheInBytes

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,9 @@ const { GenerateSW } = require("workbox-webpack-plugin");
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production' ? '/ccse-quiz/' : '/',
     configureWebpack: {
-      plugins: [new GenerateSW()]
+      plugins: [new GenerateSW({
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024 /* 7M */
+      })]
     }
   }
   
