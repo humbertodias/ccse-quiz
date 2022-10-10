61f71d44bf3e4b72769fa0cb351f8a3b9ed65f4c
Humberto Dias <hldtux@gmail.com>
2022-10-10 11:27:25 +0200

    npm run deploy

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ cd dist
 git init
 git add -A
 git commit -m 'New Deployment'
-git push -f git@github.com:humbertodias/ccse-quiz.git main:gh-pages
+git push -f https://github.com/humbertodias/ccse-quiz.git `git rev-parse HEAD`:gh-pages
 
 cd -
